d30684cd3bdebb34dd579dceee6c088b289d390e
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-10 16:26:17 +0200

    osa1 komponentille monta propsia

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,19 +1,21 @@
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
 
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" />
-      <Hello name="Pekka" />
-      <Hello name="JK" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika}/>
+      <Hello name="JK" age={2023-1982} />
     </div>
   );
 }
