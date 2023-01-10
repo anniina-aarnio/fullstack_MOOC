2b8bab0d3f2d30c5e83f60d15dbad19a34ef3935
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-10 21:35:58 +0200

    osa1 testiohjelmassa muokattu LeftRightCounteria niin että tila on olio

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -54,20 +54,37 @@ const Counter = (props) => {
 
 const LeftRightCounter = () => {
 
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
       <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
+        {clicks.left}
+        <button onClick={handleLeftClick}>
           left
         </button>
-        <button onClick={() => setRight(right + 1)}>
+        <button onClick={handleRightClick}>
           right
         </button>
-        {right}
+        {clicks.right}
       </div>
     </div>
   )
