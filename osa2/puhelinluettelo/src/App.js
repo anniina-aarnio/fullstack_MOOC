0931d1dfd9680c8bce994c005e1065b4a8d243e5
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-12 14:37:48 +0200

    osa2 2.8 lisätty numeron lisääminen, mutta se ei tarkista että on numeroina

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,23 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Testi Nimi'}
+    { name: 'Arto Hellas', number: '123 456'},
+    { name: 'Testi Nimi', number: '123 4567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleClick = (e) => {
     e.preventDefault()
-    if (persons.find((person) => person.name === newName)) {
+    if (persons.find((person) => person.name === newName.trim())) {
       window.alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({ name: newName }))
+    setPersons(persons.concat(
+      { name: newName.trim(), number: newNumber.trim() }))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -26,14 +33,25 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input
+                  value={newName}
+                  onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) =>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      )}
     </div>
   )
 
