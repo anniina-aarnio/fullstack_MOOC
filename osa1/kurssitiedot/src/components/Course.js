e1b6d106e06e239002f3f480c5445f92badd3864
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-12 10:23:22 +0200

    osa2-1 lisätty import Total Course-moduuliin

--- osa1/kurssitiedot/src/components/Course.js
+++ osa1/kurssitiedot/src/components/Course.js
@@ -1,30 +1,6 @@
 import Header from './Header'
-const Part = (props) => {
-    return (
-      <p>{props.part.name} {props.part.exercises}</p>
-    )
-}
-  
-const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-      </div>
-    )
-}
-  
-const Total = (props) => {
-    const number = props.parts[0].exercises
-                    + props.parts[1].exercises
-                    + props.parts[2].exercises
-    return (
-      <p>
-        Number of exercises {number}
-      </p>
-    )
-  }
+import Content from './Content'
+import Total from './Total'
 
 const Course = ({ course }) => {
     return (
