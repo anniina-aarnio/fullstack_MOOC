95289859c8ef37e6b4cf153693156336ff4554aa
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-12 10:35:06 +0200

    osa2-2 muutettu Total laskemaan yhteenlasketut pisteet dynaamisesti kovakoodauksen sijaan

--- osa1/kurssitiedot/src/components/Total.js
+++ osa1/kurssitiedot/src/components/Total.js
@@ -1,11 +1,11 @@
 const Total = ({ parts }) => {
-    const number = parts.reduce((sum, part) => sum + part.exercises, 0)
-    
-    return (
-      <p>
-        Number of exercises {number}
-      </p>
-    )
+  const number = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <p>
+      Number of exercises {number}
+    </p>
+  )
 }
 
 export default Total
