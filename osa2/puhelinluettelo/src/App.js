e49de81bd1097276460234e29183280ee6e9eb97
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-12 14:16:51 +0200

    osa2 2.6 lisätty toiminnallisuus, että input voi kirjoittaa ja sen sisältö tallentuu persons-tilaan kun painaa add

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setPersons(persons.concat(newName))
+    setNewName('')
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <h1>testi</h1>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
