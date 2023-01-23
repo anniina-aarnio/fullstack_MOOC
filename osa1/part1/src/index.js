6222325f370a4164a4499d2bf87b77068044dc96
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-23 09:53:20 +0200

    osa2 harjoittelu jatkuu: axios:n metodien ketjutusta

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-promise.then(response => {
-    console.log(response)
-})
+axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+        const notes = response.data
+        console.log(notes)
+    })
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
