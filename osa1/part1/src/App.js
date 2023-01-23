04ff9d2fbafad1900bc132bc0187f869e9fcf62f
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-23 11:51:28 +0200

    osa2 harjoittelu jatkuu: lisätty Noten nappiin toiminnallisuus, joka oikeasti muuttaa PUT-pyynnöllä kyseisen muistiinpanon tärkeyttä truesta falseksi tai päinvastoin

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -54,7 +54,15 @@ const App = () => {
     : notes.filter(note => note.important)
 
   const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note: response.data))
+    })
   }
 
   return (
