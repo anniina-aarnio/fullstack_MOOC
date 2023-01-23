8aa8588dd994418a52ac5a1e8464b1ad20ecb226
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-01-23 10:03:54 +0200

    osa2 harjoittelu jatkuu: axios siirretty App:n puolelle

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,10 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 import Counter from './components/Counter'
 import Hello from './components/Hello'
 import LeftRightCounter from './components/LeftRightCounter'
 
 const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+        console.log(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+
   const nimi = 'Pekka'
   const ika = 10
+
+
 
   return (
     <>
@@ -14,6 +36,7 @@ const App = () => {
       <Hello name="JK" age={2023-1982} />
       <Counter start={0} />
       <LeftRightCounter />
+      {}
     </>
   );
 }
