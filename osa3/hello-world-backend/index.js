9bd68bf38accd3d26bdfe3d7a59b07aead59c4a6
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-11 13:15:46 +0300

    osa3 hello-world added post-method

--- osa3/hello-world-backend/index.js
+++ osa3/hello-world-backend/index.js
@@ -49,10 +49,37 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+    
+    // if no body.content: return
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+    
+    notes.concat(note)
+    // console.log(note)
     response.json(note)
 })
+
+// DATAN VASTAANOTTAMINEN -otsikko MELKEIN VALMIS - tekemättä:
+// "Tämänhetkisessä versiossa on vielä se ongelma,
+// että voimme HTTP POST -pyynnöllä lisätä mitä tahansa
+// kenttiä sisältäviä olioita."
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT)
